5810ae067d84aba2ae4de12028801b19606e8589
Thomas Bird <godoploid@gmail.com>
2016-03-27 16:25:10 -0400

    added sidebar to an external file

---
+++ lib/sidebar-links.php
@@ -0,0 +1,14 @@
+<div class="module">
+    <div class="module-header">
+        <h4>Sidebar links</h4>
+    </div>
+    <div class="module-content">
+        <ul>
+            <li><a href="">Link 1</a></li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li>
+            <li><a href="">Link 4</a></li>
+            <li><a href="">Link 5</a></li>
+        </ul>
+    </div>
+</div>
--- sponsors.php
+++ sponsors.php
@@ -16,20 +16,7 @@
 
         <div class="sidebar">
 
-            <div class="module">
-                <div class="module-header">
-                    <h4>Sidebar links</h4>
-                </div>
-                <div class="module-content">
-                    <ul>
-                        <li><a href="">Link 1</a></li>
-                        <li><a href="">Link 2</a></li>
-                        <li><a href="">Link 3</a></li>
-                        <li><a href="">Link 4</a></li>
-                        <li><a href="">Link 5</a></li>
-                    </ul>
-                </div>
-            </div>
+            <?php require 'lib/sidebar-links.php'; ?>
 
         </div>
         <div class="content">
